ca2dd856105fdcf95f5c0067f785b1727c2d2ea3
Tristan Rice <rice@fn.lc>
2018-09-18 12:43:51 -0700

    added monitor script

---
+++ monitor.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+download () {
+    curl "https://dropbike.herokuapp.com/v3/bikes?lat=49.2606&lng=-123.2460" |
+        gzip -c > data/bikes-$(date -Iseconds).json.gz
+}
+
+export -f download
+
+watch -n 30 bash -c download
+
